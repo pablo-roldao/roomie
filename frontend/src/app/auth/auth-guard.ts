49abd5fcb22c8d86cc09f45d77e17646cc18b03a
João Francisco <joaofrancisco.amello@gmail.com>
2026-02-11 12:37:39 -0300

    feat(auth): criação do auth-guard.ts

--- frontend/src/app/auth/auth-guard.ts
+++ frontend/src/app/auth/auth-guard.ts
@@ -1,5 +1,18 @@
-import { CanActivateFn } from '@angular/router';
+import {CanActivateFn, Router} from '@angular/router';
+import { inject } from '@angular/core';
+import { Auth } from './auth';
 
 export const authGuard: CanActivateFn = (route, state) => {
+  const authService = inject(Auth);
+  const router = inject(Router);
+  const requiredRole = route.data?.['role'];
+
+  if(!authService.isAuthenticated()) {
+    return router.createUrlTree(['/login']);
+  }
+
+  if(requiredRole && !authService.hasRole(requiredRole)) {
+    return router.createUrlTree(['/unauthorized']);
+  }
   return true;
 };
